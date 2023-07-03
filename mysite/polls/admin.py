86a91296e630926d2733613e808e2ba03bd7f13b
Daniel Chura <dchuramo@unsa.edu.pe>
2023-07-02 21:30:10 -0500

    agregando funcion de edicion a DestinosTuristicos

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, DestinosTuristicos
+
+
+admin.site.register(Question)
+
+admin.site.register(DestinosTuristicos)
