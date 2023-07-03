b6971a1a850b257a40c1f215d1d7c1119b03f1b3
Daniel Chura <dchuramo@unsa.edu.pe>
2023-07-03 16:21:01 -0500

    comenzando pag principal

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html',{'name':'Daniel'})
+def index(request):
+    return render(request, 'index.html')
