19f7054a40fed2ad02a79f01df98677abbacfc5f
Daniel Chura <dchuramo@unsa.edu.pe>
2023-07-03 19:49:29 -0500

    revisando index.html

--- mysite/travello/models.py
+++ mysite/travello/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models
+
+class DestinosTuristicos(models.Model): 
+     # Atributos del modelo
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='destinos/')
+    precioTour = models.DecimalField(max_digits=8, decimal_places=2)
+    ofertaTour = models.BooleanField()
+
+    def __str__(self):
+        return self.nombreCiudad
+
+    class Meta:
+        app_label = 'travello'  # Etiqueta de aplicación explícita
